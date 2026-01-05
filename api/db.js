a70c8e285a6da717967bb29dc2c10f2bf2f0f695
Vkt5451 <146757279+Vkt5451@users.noreply.github.com>
2026-01-05 06:55:58 +0000

    update

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Server-side Supabase client
-const supabaseUrl = process.env.SUPABASE_URL
+const supabaseUrl = process.env.PUBLIC_SUPABASE_URL
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey)
